d28d602bc7779c218115c8102ec21daf7e8d15f0
Alex Martir Sabater <alex@venomapps.com>
2021-04-13 12:58:26 +0200

    Added: -Added style sheet to product-list.component.ts. -CustomPipe to replace any string character to an space. -Added interface product.ts in order to access product properties.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,ProductListComponent
+    AppComponent,ProductListComponent,ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,FormsModule
