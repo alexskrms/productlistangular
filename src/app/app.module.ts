a58b51e861d2ae5e781c721fd071bdc511d0a4df
Alex Martir Sabater <alex@venomapps.com>
2021-04-13 11:35:15 +0200

    First commit for the project.

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,ProductListComponent
+  ],
+  imports: [
+    BrowserModule,FormsModule
+  ],
+  bootstrap: [AppComponent,ProductListComponent]
+})
+export class AppModule { }
